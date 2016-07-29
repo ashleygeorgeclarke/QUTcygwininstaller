776db1bdec55f19feaacb9aee2a2aab5c67f2c08
ashclarke <ashleyc@appscript.com.au>
2016-07-29 21:53:08 +1000

    initial

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # QUTcygwininstaller
+
+Save this batch file to your h: drive for convenience and run it on any QUT computer to download, configure and run Cygwin properly from c: (not your slow h:)
+Added bonus of not having to set the path environmental variables
---
+++ cgywininstall.bat
@@ -0,0 +1,11 @@
+@ECHO OFF
+if not exist "h:\setup-x84.exe" (
+    START /WAIT bitsadmin.exe /transfer "cygwindl" https://cygwin.com/setup-x86.exe h:\setup-x84.exe
+)
+
+START /WAIT setup-x86.exe -q -P gcc-core,make,gdb,libncurses-devel -s http://mirror.internode.on.net
+echo Press any key when cygwin installation has finished
+pause >nul
+c:\cygwin\bin\bash.exe --login -i -c 'cd "/cygdrive/h";bash'
+
+
